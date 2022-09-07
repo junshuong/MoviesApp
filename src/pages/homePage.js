fac5bd922b4e8faf491f29e5d1d313d6c89447cf
Jun-Shuo Ng <20069422@mail.wit.ie>
2022-09-07 13:36:55 +0100

    Added routing and live data from API

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import FilterCard from "../components/filterMoviesCard";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import MovieList from "../components/movieList";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    padding: "20px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
   },
-});
+  gridList: {
+    width: 450,
+    height: "100vh",
+  },
+}));
 
 const MovieListPage = (props) => {
   const classes = useStyles();
-  const movies = props.movies;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Grid container className={classes.root}>
