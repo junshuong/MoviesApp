34320411953651c92da8fd620049398471f95a32
Jun-Shuo Ng <20069422@mail.wit.ie>
2022-09-14 06:01:34 +0100

    Fix upcoming movies page.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -77,8 +77,14 @@ export const getMovie = (args) => {
   // Added hook to GET upcoming movies 
   export const getUpcomingMovies = () => {
     return fetch(
-      `https://api.themoviedb.org/3/movie/upcoming??api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results);
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
   };
